45750cfcceabe3a48c9a636ff0737cd855d152ae
Noah Stier <noah.stier@gmail.com>
2015-08-22 00:08:09 -0700

    vectorize gradient direction cost computation

--- fLiveWireGetCostFcn.m
+++ fLiveWireGetCostFcn.m
@@ -40,75 +40,75 @@ dFd = zeros(2047);
 dX = interleave_zeros(dX);
 dY = interleave_zeros(dY);
 
-for x = 1:2:size(dX, 2)-2
-    for y = 1:2:size(dX, 1)-2
-        
-        % right
-        p = [x; y];
-        q = [x+2; y];
-        
-        Dp = [dY(x, y); -dX(x ,y)];
-        Dq = [dY(x+2, y); -dX(x+2 ,y)];
-        
-        if Dp' * (q - p) >= 0
-            Lpq = q - p;
-        else
-            Lpq = p - q;
-        end
-        
-        dppq = Dp' * Lpq;
-        dqpq = Lpq' * Dp;
-        
-        fDpq = 1/pi * (1/cos(dppq) + 1/cos(dqpq));
-        
-        dFd(x+1, y) = fDpq;
-        
-        % down
-        p = [x; y];
-        q = [x; y+2];
-        
-        Dp = [dY(x, y); -dX(x ,y)];
-        Dq = [dY(x, y+2); -dX(x, y+2)];
-        
-        if Dp' * (q - p) >= 0
-            Lpq = q - p;
-        else
-            Lpq = p - q;
-        end
-        
-        dppq = Dp' * Lpq;
-        dqpq = Lpq' * Dp;
-        
-        fDpq = 1/pi * (1/cos(dppq) + 1/cos(dqpq));
-        
-        dFd(x, y+1) = fDpq;
-        
-        % diag (down, right)
-        p = [x; y];
-        q = [x+2; y+2];
-        
-        Dp = [dY(x, y); -dX(x ,y)];
-        Dq = [dY(x+2, y+2); -dX(x+2, y+2)];
-        
-        if Dp' * (q - p) >= 0
-            Lpq = q - p;
-        else
-            Lpq = p - q;
-        end
-        
-        dppq = Dp' * Lpq;
-        dqpq = Lpq' * Dp;
-        
-        fDpq = 1/pi * (1/cos(dppq) + 1/cos(dqpq));
-        
-        dFd(x+1, y+1) = fDpq;
-    end
-end
+% the indices of dX and dY which are not newly interleaved zeros
+ind = logical(interleave_zeros(ones(size(dImg))));
+
+% gradient direction cost right
+Dp = [dY(ind), -dX(ind)];
+Dp(end-1023:end, :) = [];
+Dq = [dY(ind), -dX(ind)];
+Dq(1:1024, :) = [];
+
+Lpq = [ones(length(Dq), 1) zeros(length(Dq), 1)];
+dppq = sum(Lpq .* Dp, 2);
+dppq(dppq<0) = dppq(dppq<0) * -1;
+dqpq = sum(Lpq .* Dq, 2);
+dqpq(dqpq<0) = dqpq(dqpq<0) * -1;
+
+fDpq = 1/pi .* (ones(size(dppq)) ./ cos(dppq) + ones(size(dqpq)) ./ cos(dqpq));
+fDpq = reshape(fDpq, 1024, 1023);
+fDpq = interleave_zeros(fDpq);
+fDpq = [zeros(2047, 1) fDpq zeros(2047, 1)];
+
+dFd = dFd + fDpq;
+
+% gradient direction cost down
+
+Dp = [dY(ind), -dX(ind)];
+Dp(mod(1:1024^2, 1024)==0, :) = [];
+Dq = [dY(ind), -dX(ind)];
+Dq(mod(1:1024^2, 1024)==1, :) = [];
+
+Lpq = [zeros(length(Dq), 1) ones(length(Dq), 1)];
+dppq = sum(Lpq .* Dp, 2);
+dppq(dppq<0) = dppq(dppq<0) * -1;
+dqpq = sum(Lpq .* Dq, 2);
+dqpq(dqpq<0) = dqpq(dqpq<0) * -1;
+
+fDpq = 1/pi .* (ones(size(dppq)) ./ cos(dppq) + ones(size(dqpq)) ./ cos(dqpq));
+fDpq = reshape(fDpq, 1023, 1024);
+fDpq = interleave_zeros(fDpq);
+fDpq = [zeros(1, 2047); fDpq; zeros(1, 2047)];
+
+dFd = dFd + fDpq;
+
+% gradient direction cost diagonally down+right
+Dp = [dY(ind), -dX(ind)];
+Dp(mod(1:1024^2, 1024)==0, :) = [];
+Dp(end-1022:end, :) = [];
+
+Dq = [dY(ind), -dX(ind)];
+Dq(mod(1:1024^2, 1024)==1, :) = [];
+Dq(1:1023, :) = [];
+
+Lpq = ones(length(Dq), 2);
+dppq = sum(Lpq .* Dp, 2);
+dppq(dppq<0) = dppq(dppq<0) * -1;
+dqpq = sum(Lpq .* Dq, 2);
+dqpq(dqpq<0) = dqpq(dqpq<0) * -1;
+
+fDpq = 1/pi .* (ones(size(dppq)) ./ cos(dppq) + ones(size(dqpq)) ./ cos(dqpq));
+fDpq = reshape(fDpq, 1023, 1023);
+fDpq = interleave_zeros(fDpq);
+fDpq = [zeros(1, 2045); fDpq; zeros(1, 2045)];
+fDpq = [zeros(2047, 1) fDpq zeros(2047, 1)];
+
+dFd = dFd + fDpq;
 
 % normalize
-norm = max(max(dFd)) - min(min(dFd));
-dFd = dFd ./ norm;
+dFd(dFd==0) = min(min(dFd));
 dFd = dFd - min(min(dFd));
+dFd = dFd ./ max(max(dFd));
 
 % THE GRADIENT STRENGTH COST FG
 dImg = double(dImg);
@@ -126,6 +126,4 @@ dF = dWz.*double(lFz)+ dWg.*dFg;
 % then interleave zeros and fill in the gaps with Fd
 dF = interleave_zeros(dF);
 dF = dF + dWd .* dFd;
-
-disp('hi')
 % -------------------------------------------------------------------------
